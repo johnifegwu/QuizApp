71d7ad2126a8ca9df6272b09a304052278ed7dfe
johnifegwu <57634092+johnifegwu@users.noreply.github.com>
2021-02-06 02:30:37 +0100

    Initial commit

---
+++ app/src/main/java/com/example/quizapp/QuestionModel.java
@@ -0,0 +1,28 @@
+package com.example.quizapp;
+
+public class QuestionModel {
+
+    private String QuestionString;
+    private String Answer;
+
+    public QuestionModel(String questionString, String answer) {
+        this.QuestionString = questionString;
+        this.Answer = answer;
+    }
+
+    public String getQuestionString() {
+        return this.QuestionString;
+    }
+
+    public void setQuestionString(String questionString) {
+        this.QuestionString = questionString;
+    }
+
+    public String getAnswer() {
+        return this.Answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.Answer = answer;
+    }
+}
