14033bf7ccf31abc5592be0a10d34c34ebf02f20
John Ifegwu <johnifegwu@outlook.com>
2021-05-10 20:39:05 +0100

    Update MainActivity.java

--- app/src/main/java/com/mickleentityltdnigeria/quizapp/MainActivity.java
+++ app/src/main/java/com/mickleentityltdnigeria/quizapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.mickleentityltdnigeria.quizapp;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
